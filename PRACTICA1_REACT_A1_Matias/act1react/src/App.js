7f0c10b13b414aa1132e354f03648e366867f087
mrodriguezzsx <mrodrigu@jaumebalmes.net>
2023-02-03 17:11:27 +0100

    Activitat 1

--- PRACTICA1_REACT_A1_Matias/act1react/src/App.js
+++ PRACTICA1_REACT_A1_Matias/act1react/src/App.js
@@ -9,16 +9,20 @@ function integrantName(integrants) {
   return integrants.primerIntegrant + " i " + integrants.segonIntegrant;
 }
 
-const element = (
-  <div>
-    <h1>Grup: Daw2</h1>
-    <h2>Integrants: {integrantName(integrants)}</h2>
-  </div>
-)
-
 function App() {
   return (
-    element
+    <div>
+    <h1>Grup: Daw2</h1>
+    <h2>Integrants: {integrantName(integrants)}</h2>
+    <label for="fname">Nom:</label>
+    <input type="text" id="fname" name="fname" placeholder="Escriu el teu nom complet"></input><br></br>
+    <br></br>
+    <label for="sexe">Sexe:</label>
+    <input type="text" id="sexe" name="sexe" placeholder="Escriu el teu sexe (M/F)"></input><br></br>
+    <br></br>
+    <label for="edat">Edat:</label>
+    <input type="text" id="edat" name="edat" placeholder="Escriu la teva edat"></input><br></br>
+  </div>
   );
 }
 
