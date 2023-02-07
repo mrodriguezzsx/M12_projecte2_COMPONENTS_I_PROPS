5eacf22059c24cf3d034033a5d3763d1025364c9
mrodriguezzsx <mrodrigu@jaumebalmes.net>
2023-02-07 16:18:13 +0100

    A2

--- PRACTICA1_REACT_A1_Matias/act1react/src/App.js
+++ PRACTICA1_REACT_A1_Matias/act1react/src/App.js
@@ -1,28 +1,36 @@
 import './App.css';
 
-const integrants= {
-  primerIntegrant: 'Matias',
-  segonIntegrant: 'Mitchel'
+const Integrants = () => {
+  return (
+    <div>
+      <h1>Grup: Daw2</h1>
+      <h2>Matias Rodriguez</h2>
+      <h2>Mitchel Rosado</h2>
+    </div>
+  )
 }
 
-function integrantName(integrants) {
-  return integrants.primerIntegrant + " i " + integrants.segonIntegrant;
-}
+const Text_Info = () => {
+  return (
+    <div>
+      <label for="fname">Nom: </label>
+      <input type="text" id="fname" name="fname" placeholder="Escriu el teu nom complet"></input><br></br>
+      <br></br>
+      <label for="sexe">Sexe: </label>
+      <input type="text" id="sexe" name="sexe" placeholder="Escriu el teu sexe (M/F)"></input><br></br>
+      <br></br>
+      <label for="edat">Edat: </label>
+      <input type="text" id="edat" name="edat" placeholder="Escriu la teva edat"></input><br></br>
+    </div>
+  )
+} 
 
 function App() {
   return (
-    <div>
-    <h1>Grup: Daw2</h1>
-    <h2>Integrants: {integrantName(integrants)}</h2>
-    <label for="fname">Nom:</label>
-    <input type="text" id="fname" name="fname" placeholder="Escriu el teu nom complet"></input><br></br>
-    <br></br>
-    <label for="sexe">Sexe:</label>
-    <input type="text" id="sexe" name="sexe" placeholder="Escriu el teu sexe (M/F)"></input><br></br>
-    <br></br>
-    <label for="edat">Edat:</label>
-    <input type="text" id="edat" name="edat" placeholder="Escriu la teva edat"></input><br></br>
-  </div>
+    <div className="App">
+      <Integrants />
+      <Text_Info />
+    </div>
   );
 }
 
---
+++ PRACTICA1_REACT_A2_Matias/act2react/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Grup = (props) => {
+  return (
+    <div>
+      <h1>Grup: {props.grup}</h1>
+    </div>
+  );
+}
+
+const Integrants = (props) => {
+  return (
+    <div>
+      <h2>{props.name} {props.age} </h2>
+    </div>
+  );
+}
+
+const Text_Info = (props) => {
+  return (
+    <div>
+      <input type="text" id="fname" name="fname" placeholder="Escriu el teu nom"></input><br></br>
+      <input type="text" id="sname" name="sname" placeholder="Escriu el teu cognom"></input><br></br>
+      <input type="text" id="edad" name="edad" placeholder="Escriu la teva edad"></input><br></br>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Grup grup="Daw2"/>
+      <Integrants name="Matias Rodriguez" age={20}/>
+      <Integrants name="Mitchel Rosado" age={24}/>
+      <Text_Info/>
+    </div>
+  );
+}
+
+export default App;
